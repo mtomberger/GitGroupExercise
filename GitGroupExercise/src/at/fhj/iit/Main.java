fc9bf003b91de01c76c78a585113b28f78cc4249
weclub11 <weclub11@htlkaindorf.at>
2018-05-12 15:37:03 +0200

    implemented Main

--- GitGroupExercise/src/at/fhj/iit/Main.java
+++ GitGroupExercise/src/at/fhj/iit/Main.java
@@ -4,8 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Greeter g = new GreeterImpl("Mike Ulm");
+		Greeter g = new GreeterImpl("Markus Tomberger");
+		Greeter g1 = new GreeterImpl("Lukas Wechtitsch");
 
+		g.sayGoodbye();
+		g1.sayGoodbye()
+		
+		g.sayHello();
+		g1.sayHello();
 	}
 
 }
