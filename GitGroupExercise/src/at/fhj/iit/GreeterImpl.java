22ef7a4ccc49bf66a3329d3f77f238f6209ba562
weclub11 <weclub11@htlkaindorf.at>
2018-05-12 15:13:37 +0200

    added new class GreeterImpl

---
+++ GitGroupExercise/src/at/fhj/iit/GreeterImpl.java
@@ -0,0 +1,18 @@
+package at.fhj.iit;
+
+public class GreeterImpl implements Greeter {
+	
+	private String name;
+	
+	public GreeterImpl(String name) {
+		this.name = name;
+	}
+	
+	public void sayHello() {
+		
+	}
+	
+	public void sayGoodbye() {
+		
+	}
+}
